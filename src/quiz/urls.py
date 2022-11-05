aed4f695fae64744cdd888d80c8ca8709cfd7c29
botany <yoshikikubotani.lab@gmail.com>
2022-11-05 08:07:06 +0000

    :building_construction: Add html related files for each page

--- src/quiz/urls.py
+++ src/quiz/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TopPage.as_view(), name='home'),
+    path('', views.StartPage.as_view(), name='start'),
+    path('genra', views.GenraPage.as_view(), name='genra'),
+    path('wordlevel', views.WordLevelPage.as_view(), name='wordlevel'),
+    path('solve', views.SolvePage.as_view(), name='solve'),
+    path('result', views.ResultPage.as_view(), name='result'),
 ]
--- src/quiz/views.py
+++ src/quiz/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class TopPage(TemplateView):
-    template_name: str = "quiz/top_page.html"
+class StartPage(TemplateView):
+    template_name: str = "quiz/start_page.html"
 
     def get(self, request, *args, **kwargs):
         # getが飛んだ時の処理を書く
         return render(request, self.template_name, context=self.kwargs)
 
+class GenraPage(TemplateView):
+    template_name: str = "quiz/genra.html"
+
+    def get(self, request, *args, **kwargs):
+        # getが飛んだ時の処理を書く
+        return render(request, self.template_name, context=self.kwargs)
+
+class WordLevelPage(TemplateView):
+    template_name: str = "quiz/wordlevel.html"
+
+    def get(self, request, *args, **kwargs):
+        # getが飛んだ時の処理を書く
+        return render(request, self.template_name, context=self.kwargs)
+
+class SolvePage(TemplateView):
+    template_name: str = "quiz/solve.html"
+
+    def get(self, request, *args, **kwargs):
+        # getが飛んだ時の処理を書く
+        return render(request, self.template_name, context=self.kwargs)
+
+class ResultPage(TemplateView):
+    template_name: str = "quiz/result.html"
+
+    def get(self, request, *args, **kwargs):
+        # getが飛んだ時の処理を書く
+        return render(request, self.template_name, context=self.kwargs)
