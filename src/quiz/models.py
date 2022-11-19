039722be83950035c73cc82eeae35e70c7e1be9e
botany <yoshikikubotani.lab@gmail.com>
2022-11-19 18:22:36 +0000

    :sparkles: Make sure the html file can get the test set from the view.py context

--- src/quiz/models.py
+++ src/quiz/models.py
@@ -8,3 +8,6 @@ class MasterWordTable(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.english
