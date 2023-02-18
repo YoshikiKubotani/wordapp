cbebee0e14d88152820f6826b59c52c0f92e7a20
botany <yoshikikubotani.lab@gmail.com>
2023-02-18 06:02:36 +0000

    :sparkles: Improve the options to include the correct answer

--- src/quiz/pyfiles/maketest.py
+++ src/quiz/pyfiles/maketest.py
@@ -1,16 +1,18 @@
 import random
 
-def make_random_test(all_items, num_question):
+def make_random_test(all_items, num_questions):
     len_data = len(all_items)
     test_set_list = []
-    for i in range(num_question):
-        item_id_list = random.sample(range(len_data), k=5)
+    for i in range(num_questions):
+        item_id_list = random.sample(range(len_data), k=4)
+        answer_id = random.randint(1, 4)
         question_dict = {}
         for idx, item_id in enumerate(item_id_list):
-            if idx == 0:
+            if idx+1 == answer_id:
                 question_dict["question_en"] = all_items[item_id].english
                 question_dict["answer_jp"] = all_items[item_id].japanese
+                question_dict["option_{}".format(idx+1)] = all_items[item_id].japanese
             else:
-                question_dict["option_{}".format(idx)] = all_items[item_id].japanese
+                question_dict["option_{}".format(idx+1)] = all_items[item_id].japanese
         test_set_list.append(question_dict)
     return test_set_list
