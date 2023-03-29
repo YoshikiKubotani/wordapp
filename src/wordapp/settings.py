34b614a4bcebd3b862f0d7b11eb4fcd6d3cfb60d
botany <yoshikikubotani.lab@gmail.com>
2023-03-29 09:15:41 +0000

    :wrench: Add localhost to ALLOWED_HOSTS

--- src/wordapp/settings.py
+++ src/wordapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--fpu(=d7vxli8ftwnrm4i@w9nh#mc*$(&v7y=#=c1h8^)f8be0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
