ca8fa01765e543718b997879f525c3ae748cd0dc
botany <yoshikikubotani.lab@gmail.com>
2023-05-02 09:16:07 +0000

    :sparkles: Update solve page to display the result on each attempt

--- src/quiz/static/quiz/scripts/solve.js
+++ src/quiz/static/quiz/scripts/solve.js
@@ -7,19 +7,19 @@ const QuestionComponent = {
     <h1>👋 問題👋</h1>
     <div class="question"> [[ question ]] </div>
     <fieldset>
-        <input id="option-1" class="radio-inline__input" type="radio" name="solve-choice[[ count ]]" value="[[ op1 ]]" checked="checked"/>
+        <input id="option-1" class="radio-inline__input" type="radio" name="solve-choice[[ count ]]" v-model="radio" v-bind:value="op1" checked="checked"/>
         <label class="radio-inline__label" for="option-1">
             [[ op1 ]]
         </label>
-        <input id="option-2" class="radio-inline__input" type="radio" name="solve-choice[[ count ]]" value="[[ op2 ]]"/>
+        <input id="option-2" class="radio-inline__input" type="radio" name="solve-choice[[ count ]]" v-model="radio" v-bind:value="op2"/>
         <label class="radio-inline__label" for="option-2">
             [[ op2 ]]
         </label>
-        <input id="option-3" class="radio-inline__input" type="radio" name="solve-choice[[ count ]]" value="[[ op3 ]]"/>
+        <input id="option-3" class="radio-inline__input" type="radio" name="solve-choice[[ count ]]" v-model="radio" v-bind:value="op3"/>
         <label class="radio-inline__label" for="option-3">
             [[ op3 ]]
         </label>
-        <input id="option-4" class="radio-inline__input" type="radio" name="solve-choice[[ count ]]" value="[[ op4 ]]"/>
+        <input id="option-4" class="radio-inline__input" type="radio" name="solve-choice[[ count ]]" v-model="radio" v-bind:value="op4"/>
         <label class="radio-inline__label" for="option-4">
             [[ op4 ]]
         </label>
@@ -33,6 +33,16 @@ const QuestionComponent = {
         </a>
     </div>
     <div id="switch" v-if="showButtons">
+        <div id="answer_text" class="center" v-if="isCorrect">
+            正解
+        </div>
+        <div id="display_text" v-if="!isCorrect">
+            <div class="left">不正解！正解は</div>
+            <div id="answer_text" class="center">
+                「[[ answer ]]」
+            </div>
+            <div class="right">です。</div>
+        </div>
         <a href="#" class="btn btn-flat" @click=prevItem><span>前の問題</span></a>
         <a href="#" class="btn btn-flat" @click=nextItem><span>次の問題</span></a>
     </div>
@@ -46,6 +56,8 @@ const QuestionComponent = {
         const op2 = Vue.ref(props.questionSet[count.value]["option_2"])
         const op3 = Vue.ref(props.questionSet[count.value]["option_3"])
         const op4 = Vue.ref(props.questionSet[count.value]["option_4"])
+        const radio = Vue.ref(op1.value)
+        const isCorrect = Vue.ref(false)
         console.log(count.value)
 
         const nextItem = () => {
@@ -60,6 +72,7 @@ const QuestionComponent = {
                 showButtons.value = false
                 console.log(count.value)
                 console.log(question.value)
+                radio.value = op1.value
             }
         };
 
@@ -75,10 +88,21 @@ const QuestionComponent = {
                 showButtons.value = false
                 console.log(count.value)
                 console.log(question.value)
+                radio.value = op1.value
             }
         };
 
-        const showSwitch = () => { showButtons.value = true }
+        const showSwitch = () => {
+            console.log(radio.value)
+            console.log(answer.value)
+            if (radio.value == answer.value) {
+                isCorrect.value = true
+            }
+            else {
+                isCorrect.value = false
+            }
+            showButtons.value = true
+        }
 
         return {
             showButtons,
@@ -88,6 +112,8 @@ const QuestionComponent = {
             op2,
             op3,
             op4,
+            radio,
+            isCorrect,
             nextItem,
             prevItem,
             showSwitch
