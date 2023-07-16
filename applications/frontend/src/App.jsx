7fbe69a88cfb02dc8c2579667ae2277e9bd6ac7c
botany <yoshikikubotani.lab@gmail.com>
2023-07-16 16:12:17 +0900

    :sparkles: Set the global style as a theme

--- applications/frontend/src/App.jsx
+++ applications/frontend/src/App.jsx
@@ -1,4 +1,7 @@
+import "@fontsource/noto-sans-jp"
+
 import React from "react";
+import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { ModeSelection } from "./pages/ModeSelection";
 import { Result } from "./pages/Result";
@@ -6,12 +9,14 @@ import { Solve } from "./pages/Solve";
 import { StartPage } from "./pages/StartPage";
 import { WordLevel } from "./pages/WordLevel";
 import { NoMatch } from "./pages/NoMatch";
+import theme from "./theme";
 
 import './App.css';
 
 function App() {
   return (
     // <AppProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<StartPage />} />
@@ -22,6 +27,7 @@ function App() {
           <Route path="*" element={<NoMatch />} />
         </Routes>
       </BrowserRouter>
+    </ChakraProvider>
     // </AppProvider>
   );
 }
--- applications/frontend/src/pages/StartPage/StartPage.jsx
+++ applications/frontend/src/pages/StartPage/StartPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./StartPage.css";
+import { Box, Text } from '@chakra-ui/react'
 
 export const StartPage = ({}) => {
     return (
-        <div></div>
+        <Box>
+            <Text fontSize='3xl'>英単語アプリWelcome</Text>
+        </Box>
     );
 };
