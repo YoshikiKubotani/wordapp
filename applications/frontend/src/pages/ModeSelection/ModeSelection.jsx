fa42a6abf9ac84b4c6976a49320c84dfffc9b4cf
botany <yoshikikubotani.lab@gmail.com>
2023-07-29 18:35:19 +0900

    :art: Update flex description

--- applications/frontend/src/pages/ModeSelection/ModeSelection.jsx
+++ applications/frontend/src/pages/ModeSelection/ModeSelection.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { Box, Flex, Heading, Button, Spacer} from '@chakra-ui/react'
+import { Box, Flex, Heading, Button} from '@chakra-ui/react'
+import { useNavigate } from "react-router-dom";
 
 const ModeSelection = ({}) => {
+  const navigate = useNavigate();
+  
   return (
-    <Flex minH="100vh" direction="column" align='center' gap='30px'>
-      <Spacer />
+    <Flex minH="100vh" direction="column" align='center' justify='center' gap='30px'>
       <Heading as='h1' size='xl'>英単語アプリ</Heading>
       <Box>
-        <Flex minW="100%" flexDirection='row' gap='10px'>
-          <Spacer />
-          <Button size='lg' colorScheme='green'>学年</Button>
+        <Flex minW="100%" direction='row' align='center' gap='10px'>
+          <Button size='lg' colorScheme='green'onClick={() => navigate("/wordlevel")}>学年</Button>
           <Button size='lg' colorScheme='green'>ジャンル</Button>
-          <Spacer />
         </Flex>
       </Box>
-      <Spacer />
     </Flex>
   );
 };
