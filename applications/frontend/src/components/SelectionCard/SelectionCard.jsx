90985455336e2960e02109060d99924166c242db
botany <yoshikikubotani.lab@gmail.com>
2023-07-30 10:37:33 +0900

    :sparkles: Add grade selection cards

--- applications/frontend/src/components/SelectionCard/SelectionCard.jsx
+++ applications/frontend/src/components/SelectionCard/SelectionCard.jsx
@@ -14,46 +14,58 @@ import {
   Divider,
 } from '@chakra-ui/react'
 import { StarIcon } from '@chakra-ui/icons'
+import { useNavigate } from "react-router-dom";
 
 const SelectionCard = ({
-  image_source,
+  imageSource,
   title,
   description,
   trial,
-  selection_text,
+  selectionText,
+  navigateTo,
 }) => {
+  const navigate = useNavigate();
+
   return (
-    <Card maxW='sm'>
-    <CardBody>
-      <Image
-        src={image_source}
-        borderRadius='lg'
-      />
-      <Stack mt='6' spacing='3'>
-        <Heading size='md'>{title}</Heading>
-        <Text>
-          {description}
-        </Text>
-        <Text color='blue.600' fontSize='2xl'>
-          {trial}
-        </Text>
-      </Stack>
-    </CardBody>
-    <Divider />
-    <CardFooter justify='space-between'>
-        <Button variant='solid' color='green.100' bg='green.600'>
-          {selection_text}
-        </Button>
-        <IconButton
-          isRound={true}
-          variant='solid'
-          colorScheme='teal'
-          aria-label='Done'
-          fontSize='20px'
-          icon={<StarIcon />}
+    <Card>
+      <CardBody>
+        <Image
+          src={imageSource}
+          borderRadius='lg'
         />
-    </CardFooter>
-  </Card>
+        <Stack mt='6' spacing='3'>
+          <Heading size='lg' color='teal.50'>{title}</Heading>
+          <Text color='teal.50'>
+            {description}
+          </Text>
+          <Text color='blue.300' fontSize='sm'>
+            {trial}回 学習済み
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter justify='space-between'>
+          <Button
+            variant='solid'
+            color='teal.50'
+            bg='teal.500'
+            _hover={{ bg: 'teal.600' }}
+            onClick={() => navigate({navigateTo})}
+          >
+            {selectionText}
+          </Button>
+          <IconButton
+            isRound={true}
+            variant='solid'
+            color='yellow.400'
+            bg='gray.500'
+            _hover={{ bg: 'gray.600' }}
+            aria-label='Done'
+            fontSize='20px'
+            icon={<StarIcon />}
+          />
+      </CardFooter>
+    </Card>
   )
 }
 
--- applications/frontend/src/pages/WordLevel/WordLevel.jsx
+++ applications/frontend/src/pages/WordLevel/WordLevel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, Heading} from '@chakra-ui/react'
+import { Box, Flex, Heading, SimpleGrid } from '@chakra-ui/react'
 import { useNavigate } from "react-router-dom";
 
 import SelectionCard from '../../components/SelectionCard'
@@ -9,31 +9,42 @@ export const WordLevel = ({}) => {
 
   return (
     <Flex minH="100vh" direction="column" align='center' justify='center' gap='30px'>
-      <Heading as='h1' size='md'>学年を選択する</Heading>
-      <Box>
-        <Flex minW="100%" direction='row' align='center' gap='10px'>
+      <Heading as='h1' size='md'>どの学年の単語を勉強する？</Heading>
+      <Box marginX="30px">
+        <SimpleGrid minChildWidth='xs' spacing='10px'>
           <SelectionCard
-            image_source="nazotoki.jpg"
-            title="test"
+            imageSource="nazotoki.jpg"
+            title="中学３年生"
             description="これはサンプルテキストです。"
             trial="4"
-            selection_text="選択"
+            selectionText="選択"
+            navigateTo="/grade/3"
           />
           <SelectionCard
-            image_source="nazotoki.jpg"
-            title="test"
+            imageSource="nazotoki.jpg"
+            title="高校１年生"
             description="これはサンプルテキストです。"
             trial="4"
-            selection_text="選択"
+            selectionText="選択"
+            navigateTo="/grade/4"
           />
           <SelectionCard
-            image_source="nazotoki.jpg"
-            title="test"
+            imageSource="nazotoki.jpg"
+            title="高校２年生"
             description="これはサンプルテキストです。"
             trial="4"
-            selection_text="選択"
+            selectionText="選択"
+            navigateTo="/grade/5"
           />
-        </Flex>
+          <SelectionCard
+            imageSource="nazotoki.jpg"
+            title="高校３年生"
+            description="これはサンプルテキストです。"
+            trial="4"
+            selectionText="選択"
+            navigateTo="/grade/6"
+          />
+        </SimpleGrid>
       </Box>
     </Flex>
   );
