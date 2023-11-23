22bab69e5de38afdc4f26ef9a180a32e5fdb0627
botany <yoshikikubotani.lab@gmail.com>
2023-11-24 00:51:24 +0900

    :sparkles: Add db management directory

---
+++ db/init/init.sql
@@ -0,0 +1,16 @@
+-- dev スキーマが存在しない場合に作成
+CREATE SCHEMA IF NOT EXISTS dev;
+
+-- item テーブルが存在しない場合に作成
+CREATE TABLE IF NOT EXISTS dev.item (
+    id INT PRIMARY KEY,
+    english TEXT,
+    japanese TEXT,
+    grade INT
+);
+
+-- 既存のデータを削除
+TRUNCATE TABLE dev.item;
+
+-- 新しいデータを挿入
+\COPY dev.item (id, english, japanese, grade) FROM '/docker-entrypoint-initdb.d/sokutan.csv' WITH (FORMAT csv, DELIMITER ',', HEADER, ENCODING 'UTF8');
