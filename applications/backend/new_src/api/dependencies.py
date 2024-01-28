560afcb98f6d56865bfc83e7f8f17ee9a51a4f9c
botany <yoshikikubotani.lab@gmail.com>
2024-01-29 01:55:05 +0900

    :sparkles: Add dependency injection for database session

--- applications/backend/new_src/api/dependencies.py
+++ applications/backend/new_src/api/dependencies.py
@@ -1,26 +1,27 @@
-from typing import Annotated, Generator
+from typing import Annotated, AsyncGenerator
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from pydantic import ValidationError
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from new_src.api.schemas import DummyUser, TokenPayload, User
 from new_src.core.config import settings
+from new_src.core.main import AsyncSessionFactory
 
 reusable_oauth2 = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/login/access-token")
 
-# def get_db() -> Generator:
-#     with Session(engine) as session:
-#         yield session
+async def get_db_session() -> AsyncGenerator[AsyncSession]:
+    async with AsyncSessionFactory() as session:
+        yield session
 
-
-# SessionDep = Annotated[Session, Depends(get_db)]
+AsyncSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
 TokenDep = Annotated[str, Depends(reusable_oauth2)]
 
 
-def get_current_user(
-    # session: SessionDep,
+async def get_current_user(
+    async_session: AsyncSessionDep,
     token: TokenDep,
 ) -> User:
     try:
@@ -55,7 +56,7 @@ def get_current_user(
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
-def get_current_active_superuser(current_user: CurrentUser) -> User:
+async def get_current_active_superuser(current_user: CurrentUser) -> User:
     if not current_user.is_superuser:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
