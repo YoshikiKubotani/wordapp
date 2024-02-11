b54e319a7ab9c063b8c6c5e5148f8c2c7d133f20
botany <yoshikikubotani.lab@gmail.com>
2024-02-12 02:08:09 +0900

    :sparkles: Add the function to create superuser for test

--- applications/backend/tests/src/utils.py
+++ applications/backend/tests/src/utils.py
@@ -38,3 +38,28 @@ async def create_random_test_user(async_session: AsyncSession, email: str) -> Us
         )
         user = await user_repository.create(async_session, user_in)
     return user
+
+async def create_test_superuser(async_session: AsyncSession) -> User:
+    """Create a superuser in the database for testing.
+
+    Args:
+        async_session (AsyncSession): The SQLAlchemy async session.
+
+    Returns:
+        User: The superuser that was created.
+    """
+    user_repository = UserRepository()
+
+    # Check if the user already exists.
+    user = await user_repository.read_by_email(async_session, email=settings.FIRST_SUPERUSER_EMAIL)
+    # Create a superuser if it doesn't exist.
+    if user is None:
+        print('Creating a superuser for testing.')
+        user_in = User(
+            user_name=settings.FIRST_SUPERUSER,
+            email=settings.FIRST_SUPERUSER_EMAIL,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True
+        )
+        user = await user_repository.create(async_session, user_in)
+    return user
