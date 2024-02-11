7107680b16a319e5f23c8fec603af7f0e213d77c
botany <yoshikikubotani.lab@gmail.com>
2024-02-11 13:05:10 +0900

    :sparkles: Add password hashing method

--- applications/backend/src/core/security.py
+++ applications/backend/src/core/security.py
@@ -13,6 +13,8 @@ password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_context.verify(plain_password, hashed_password)
 
+def get_password_hash(password: str) -> str:
+    return password_context.hash(password)
 
 async def authenticate_user(
     async_session: AsyncSessionDep,
