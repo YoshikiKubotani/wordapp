c6e9077f46fce98118e050368ecfdb027f5ce019
botany <yoshikikubotani.lab@gmail.com>
2024-02-12 02:55:41 +0900

    :speech_balloon: Remove unused commented parts

--- applications/backend/tests/conftest.py
+++ applications/backend/tests/conftest.py
@@ -16,15 +16,6 @@ server_url = f"http://localhost:8000{settings.API_V1_STR}/"
 
 settings.POSTGRES_SCHEMA = "test"
 settings.TEST_USER_EMAIL = random_email()
-# def setup_class_attributes(target_cls: Any, config: dict[str, str]) -> set[str]:
-#     found_attributes: set = set()
-#     if hasattr(target_cls, "fixture_overridden_attribute_names"):
-#         for attribute in target_cls.fixture_overridden_attribute_names:
-#             if attribute in config:
-#                 setattr(target_cls, attribute, config[attribute])
-#                 found_attributes.add(attribute)
-#     unfound_attributes = set(config.keys()) - found_attributes
-#     return unfound_attributes
 
 @pytest.fixture(scope="session")
 def anyio_backend() -> None:
@@ -86,11 +77,13 @@ async def normal_async_test_client(async_test_client) -> AsyncIterator[AsyncClie
     async with AsyncSessionFactory() as async_session:
         # Create a normal user for testing if it does not exist.
         normal_test_user = await create_random_test_user(async_session, settings.TEST_USER_EMAIL)
+
     # Log in as the normal user.
     response = await async_test_client.post(
         "/login/access-token",
         data={"username": normal_test_user.user_name, "password": normal_test_user.password},
     )
+
     token = response.json()["access_token"]
     token_type = response.json()["token_type"]
     async_test_client.headers = {"Authorization": f"{token_type} {token}"}
@@ -101,34 +94,6 @@ async def normal_async_test_client(async_test_client) -> AsyncIterator[AsyncClie
     # Drop all tables defined as data models under `src/db` after the test is done.
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
-
-
-# @pytest.fixture(scope="function")
-# async def wrapped_normal_async_test_client(normal_async_test_client) -> AsyncIterator[AsyncClient]:
-#     """Create an asynchronous test client with table startup/cleanup events.
-
-#     Each test must be independent, necessitating the initialization of database tables for every test.
-#     Although the database is initialized within the lifespan event called by the lifespan manager
-#     at the startup of the FastAPI application, this is achieved through a session-scoped fixture,
-#     requiring redefinition at the function scope as well. The reason the lifespan event must also be used
-#     in tests is that the AsyncSessionFactory created within the lifespan event is utilized in the test target
-#     FastAPI endpoints through dependency injection.
-
-#     Yields:
-#         AsyncClient: An asynchronous test client.
-#     """
-#     engine = normal_async_test_client.app.state.engine
-
-#     # Create all tables defined as data models under `src/db` if they do not exist.
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-
-#     # Yield the test client.
-#     yield normal_async_test_client
-
-#     # Drop all tables defined as data models under `src/db` after the test is done.
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.drop_all)
 
 
 @pytest.fixture(scope="function")
@@ -197,37 +162,3 @@ async def async_db_session(lifespan_manager: LifespanManager) -> AsyncIterator[A
     # Drop all tables defined as data models under `src/db` after the test is done.
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
-
-
-
-
-# @pytest.fixture(scope="class")
-# async def authenticated_async_test_client(async_test_client):
-#     """Create an authenticated user for testing."""
-#     response = await async_test_client.post(
-#         "/login/access-token",
-#         data={"username": settings.FIRST_SUPERUSER, "password": settings.FIRST_SUPERUSER_PASSWORD},
-#     )
-#     print("response", response.json())
-#     token = response.json()["access_token"]
-#     token_type = response.json()["token_type"]
-#     async_test_client.headers = {"Authorization": f"{token_type} {token}"}
-#     yield async_test_client
-
-
-# @pytest.fixture(scope="class")
-# async def assingn_authenticated_async_test_client_to_class(
-#     request, authenticated_async_test_client
-# ):
-#     """Assign the authenticated client to the class.
-
-#     This is used to override the client fixture in the test class.
-#     """
-#     unfound_attributes = setup_class_attributes(
-#         request.cls, {"client": authenticated_async_test_client}
-#     )
-#     if "client" in unfound_attributes:
-#         raise ValueError(
-#             'The test class must have "fixture_overridden_attribute_names" attribute which contains "client" to assign authenticated async test client.'
-#         )
-#     yield
