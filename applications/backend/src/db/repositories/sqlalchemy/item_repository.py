6bbde9e5bb522ba8161c6bbe6c85c20a29f9e5dd
botany <yoshikikubotani.lab@gmail.com>
2024-02-21 01:17:35 +0900

    :sparkles: Add a unit testing function for `ItemRepository.read_by_user_id`

--- applications/backend/src/db/repositories/sqlalchemy/item_repository.py
+++ applications/backend/src/db/repositories/sqlalchemy/item_repository.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.models.sqlalchemy_data_models import SQLAlchemyItem
+from src.db.models.sqlalchemy_data_models import SQLAlchemyItem, orm_object_to_dict
 from src.domain.models import Item
 
 from .base_repository import BaseRepository
@@ -15,7 +15,9 @@ class ItemRepository(BaseRepository[SQLAlchemyItem, Item]):
         # This context automatically calls async_session.commit() if no exceptions are raised.
         # If an exception is raised, it automatically calls async_session.rollback().
         async with self.async_session.begin():
-            items = await self.async_session.execute(
-                select(self.data_model).where(self.data_model.user_id == user_id)
+            results = await self.async_session.execute(
+                select(self.data_model).where(self.data_model.user_id == user_id).order_by(self.data_model.item_id)
             )
-            return items.scalars().all()
+            items = results.scalars().all()
+        items = [Item.model_validate(orm_object_to_dict(item)) for item in items]
+        return items
