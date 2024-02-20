5cfa0d2b47210cb80889dd5e389d5071accb05f6
botany <yoshikikubotani.lab@gmail.com>
2024-02-21 00:22:15 +0900

    :speech_balloon: Update test comments

--- applications/backend/tests/src/unit/db/repositories/sqlalchemy/test_deck_repository.py
+++ applications/backend/tests/src/unit/db/repositories/sqlalchemy/test_deck_repository.py
@@ -14,20 +14,24 @@ async def test_real_all(async_db_session: AsyncSession) -> None:
         async_db_session (AsyncSession): An asynchronous database session.
     """
     # Create a user and two decks for tesing.
-    test_user_domain_model = User(user_id=1, user_name="dummy_user", email="dummy_email", password="dummy_password")
-    deck1_domain_model = Deck(deck_id=1, user_id=1, deck_name="dummy_deck")
-    deck2_domain_model = Deck(deck_id=2, user_id=1, deck_name="dummy_deck")
-    test_user_data_model = SQLAlchemyUser(**test_user_domain_model.model_dump())
+    user_domain_model = User(user_id=1, user_name="dummy_user", email="dummy_email", password="dummy_password")
+    deck1_domain_model = Deck(deck_id=1, user_id=1, deck_name="dummy_deck1")
+    deck2_domain_model = Deck(deck_id=2, user_id=1, deck_name="dummy_deck2")
+    user_data_model = SQLAlchemyUser(**user_domain_model.model_dump())
     deck1_data_model = SQLAlchemyDeck(**deck1_domain_model.model_dump())
     deck2_data_model = SQLAlchemyDeck(**deck2_domain_model.model_dump())
     async with async_db_session.begin():
-        async_db_session.add_all([deck1_data_model, deck2_data_model, test_user_data_model])
+        async_db_session.add_all([user_data_model, deck1_data_model, deck2_data_model])
         await async_db_session.flush()
 
-    # Test the `DeckRepository.read_all` method.
+    # Instantiate the `DeckRepository` class.
     deck_repository = DeckRepository(async_db_session)
+    # Get all the decks.
     decks = await deck_repository.read_all()
+    # Test if the returned decks are correct (i.e. equals to the ones created above).
     assert len(decks) == 2
+    assert decks[0] == deck1_domain_model
+    assert decks[1] == deck2_domain_model
 
 
 async def test_read_by_user_id(async_db_session: AsyncSession) -> None:
