656658a6a43862c22ab640141705bc43e9c47765
botany <yoshikikubotani.lab@gmail.com>
2024-02-25 19:00:51 +0900

    :art: Refactor `items.py`

--- applications/backend/src/api/routers/items.py
+++ applications/backend/src/api/routers/items.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from src.api.dependencies import async_session_dependency, current_user_dependency
 from src.api.schemas import CreateItemRequest, ItemResponse, UpdateItemRequest
@@ -13,7 +13,15 @@ async def search_items(
     current_user: current_user_dependency,
     async_session: async_session_dependency,
 ) -> Any:
-    """Search for items."""
+    """Search for items.
+
+    Args:
+        current_user (User): The current user.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        list[ItemResponse]: The list of serached items.
+    """
     items = [
         ItemResponse(
             item_id=1, english="dummy_english", japanese="dummy_japanese", grade=1
@@ -24,11 +32,20 @@ async def search_items(
 
 @router.post("/", response_model=ItemResponse)
 async def create_item(
-    # item: CreateItemRequest,
+    item: CreateItemRequest,
     current_user: current_user_dependency,
     async_session: async_session_dependency,
 ) -> Any:
-    """Create a new item."""
+    """Create a new item.
+
+    Args:
+        item (CreateItemRequest): The item to create.
+        current_user (User): The current user.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        ItemResponse: The created item.
+    """
     return ItemResponse(
         item_id=1, english="dummy_english", japanese="dummy_japanese", grade=1
     )
@@ -40,7 +57,16 @@ async def read_item(
     current_user: current_user_dependency,
     async_session: async_session_dependency,
 ) -> Any:
-    """Get the details of an item."""
+    """Get the details of an item.
+
+    Args:
+        item_id (int): The item id.
+        current_user (User): The current user.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        ItemResponse: The item details.
+    """
     return ItemResponse(
         item_id=item_id, english="dummy_english", japanese="dummy_japanese", grade=1
     )
@@ -49,11 +75,21 @@ async def read_item(
 @router.put("/{item_id}", response_model=ItemResponse)
 async def update_item(
     item_id: int,
-    # item: UpdateItemRequest,
+    item: UpdateItemRequest,
     current_user: current_user_dependency,
     async_session: async_session_dependency,
 ) -> Any:
-    """Update the details of an item."""
+    """Update the details of an item.
+
+    Args:
+        item_id (int): The item id.
+        item (UpdateItemRequest): The item details to update.
+        current_user (User): The current user.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        ItemResponse: The updated item details.
+    """
     return ItemResponse(
         item_id=item_id, english="dummy_english", japanese="dummy_japanese", grade=1
     )
@@ -65,5 +101,14 @@ async def delete_item(
     current_user: current_user_dependency,
     async_session: async_session_dependency,
 ) -> bool:
-    """Delete an item."""
+    """Delete an item.
+
+    Args:
+        item_id (int): The item id.
+        current_user (User): The current user.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        bool: True if the item was deleted successfully.
+    """
     return True
