9455db6a2f67088f7378c2818e1be208e2502f21
botany <yoshikikubotani.lab@gmail.com>
2024-02-25 16:46:09 +0900

    :sparkles: Add an interface for `DeckRepository`

---
+++ applications/backend/src/db/repositories/deck_repository_interface.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+from src.domain.models import Deck
+
+from .base_repository_interface import IBaseRepository
+
+class IDeckRepository(IBaseRepository[Deck], ABC):
+    @abstractmethod
+    async def read_all(self) -> list[Deck]:
+        pass
+
+    @abstractmethod
+    async def read_by_user_id(self, user_id: int) -> list[Deck]:
+        pass
