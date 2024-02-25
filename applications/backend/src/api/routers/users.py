0a4f8f19f37f9a913358a841057c7b641f5fc02d
botany <yoshikikubotani.lab@gmail.com>
2024-02-25 19:09:01 +0900

    :art: Refactor `users.py`

--- applications/backend/src/api/routers/users.py
+++ applications/backend/src/api/routers/users.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
 from src.api.dependencies import (
     async_session_dependency,
@@ -20,8 +20,22 @@ router = APIRouter()
 async def read_all_users(
     async_session: async_session_dependency,
 ) -> Any:
-    """Get all users."""
-    users = [UserResponse(user_id=1, email="dummy_user")]
+    """Get all users.
+
+    Args:
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        list[UserResponse]: The list of all users.
+    """
+    users = [
+        UserResponse(
+            user_id=1,
+            user_name="dummy_user_name",
+            email="dummy_user",
+            full_name="dummy_full_name",
+        )
+    ]
     return users
 
 
@@ -31,37 +45,81 @@ async def read_all_users(
     response_model=UserResponse,
 )
 async def create_user(
-    # user: CreateUserRequest,
+    user: CreateUserRequest,
     async_session: async_session_dependency,
 ) -> Any:
-    """Create a new user."""
-    return UserResponse(user_id=1, email="dummy_user")
+    """Create a new user.
+
+    Args:
+        user (CreateUserRequest): The user to create.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        UserResponse: The created user.
+    """
+    return UserResponse(
+        user_id=1,
+        user_name="dummy_user_name",
+        email="dummy_user",
+        full_name="dummy_full_name",
+    )
 
 
 @router.get("/{user_name}", response_model=UserResponse)
 async def read_own_user(
-    # user_name: str,
+    user_name: str,
     currenct_user: current_user_dependency,
     async_session: async_session_dependency,
 ) -> Any:
-    """Get the details of a user."""
+    """Get the details of a user.
+
+    Args:
+        user_name (str): The user name.
+        currenct_user (User): The current user.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        UserResponse: The user details.
+    """
     return currenct_user
 
 
 @router.put("/{user_name}", response_model=UserResponse)
 async def update_own_user(
-    # user_name: str,
-    # user: UpdateUserRequest,
+    user_name: str,
+    user: UpdateUserRequest,
     async_session: async_session_dependency,
 ) -> Any:
-    """Update the details of a user."""
-    return UserResponse(user_id=1, email="dummy_user")
+    """Update the details of a user.
+
+    Args:
+        user_name (str): The user name.
+        user (UpdateUserRequest): The user details to update.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        UserResponse: The updated user details.
+    """
+    return UserResponse(
+        user_id=1,
+        user_name="dummy_user_name",
+        email="dummy_user",
+        full_name="dummy_full_name",
+    )
 
 
 @router.delete("/{user_name}")
 async def delete_own_user(
-    # user_name: str,
+    user_name: str,
     async_session: async_session_dependency,
 ) -> bool:
-    """Delete a user."""
+    """Delete a user.
+
+    Args:
+        user_name (str): The user name.
+        async_session (AsyncSession): The async session.
+
+    Returns:
+        bool: True if the user was deleted successfully.
+    """
     return True
