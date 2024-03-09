73b3d9593b003413784db8b1adf241c09edfa1d8
botany <yoshikikubotani.lab@gmail.com>
2024-03-09 16:48:48 +0900

    :art: Move repository tests under `integration`

--- applications/backend/tests/integration/conftest.py
+++ applications/backend/tests/integration/conftest.py
@@ -1,12 +1,37 @@
 import pytest
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.config import settings
 from src.db.models.sqlalchemy_data_models import Base
 from tests.utils import create_random_test_user, create_test_superuser
 
 server_url = f"http://localhost:8000{settings.API_V1_STR}/"
+
+
+@pytest.fixture(scope="function")
+async def async_db_session(
+    lifespan_manager: LifespanManager,
+) -> AsyncSession:
+    """Provide an asynchronous database session.
+
+    This fixture is used for testing which does not require an asynchronous test client.
+
+    Yields:
+        AsyncSession: An asynchronous database session.
+    """
+    # Import `async_session_factory` and `engine` here to make sure the lifespan manager is executed before creating the session.
+    from src.core.main import async_session_factory, engine
+
+    # Drop and create all tables defined as data models under `src/db`.
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    # This context automatically calls async_session.close() when the code block is exited.
+    async with async_session_factory() as async_session:
+        yield async_session
 
 
 @pytest.fixture(scope="class")
